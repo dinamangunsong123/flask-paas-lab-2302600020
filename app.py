42c93bee15aee7bb2b7a628e587a69957f6fac63
dinamangunsong123 <dinamangunsong935@gmail.com>
2025-11-04 15:02:46 +0700

    app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(name)
 
 @app.route('/')
 def home():
-    return "Hello from Flask deployed on Render 213510321!"
+    return "Hello from Flask deployed on Render 2302600020!"
 
 if name == 'main':
     app.run(host='0.0.0.0', port=5000)
