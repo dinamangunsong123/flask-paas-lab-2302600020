c7fd1b909bb6074b653181f04c495b779e51197e
dinamangunsong123 <dinamangunsong935@gmail.com>
2025-11-04 16:26:43 +0700

    app.py Update dina mangunsong- Initial Flask app for Render PaaS - 2302600020

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello from Flask deployed on Render 2302600020! dina mangunsong update!"
+    return "Hello from Flask deployed on Render 2302600020! dina mangunsong  update!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
